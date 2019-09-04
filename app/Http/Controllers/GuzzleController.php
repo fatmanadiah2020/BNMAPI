1fac31e44e4125b13d7cf583f0d5e11b97464990
unknown <ibrahimmerhi@mail.ru>
2019-09-04 12:46:35 +0800

    bank api

--- app/Http/Controllers/GuzzleController.php
+++ app/Http/Controllers/GuzzleController.php
@@ -3,30 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Client;
 
 
 class GuzzleController extends Controller
 {
     public function getRemoteData(){
 
-    	$client = new GuzzleClient([
+    	$headers = [
+            'Accept' => "application/vnd.BNM.API.v1+json",
+            'User-Agent'=>'PostmanRuntime/7.16.3',
+        ];
 
-    		'headers' => ['content-type' => 'application/json' ,'Accept' => 'application/vnd.BNM.API.v1+json']
-    	]);
+        $client = new Client([
+            'headers' => $headers
+        ]);
 
-		$response =$client->request('GET','https://api.bnm.gov.my/public/base-rate',[
-			'json' => [
-						'code' => '',
-					 ]
+        $r = $client->request('GET', 'https://api.bnm.gov.my/public/fx-turn-over');
+        $response = $r->getBody()->getContents();
+         echo $response;
+    	
 
-		]);
+    
+    /*
+    $client = new GuzzleClient();
+    $response = $client->request('GET', 'https://api.bnm.gov.my/public/base-rate', [
+    'headers' => [
+        'Accept' => 'application/vnd.BNM.API.v1+json',
+    ]
+    
+]);
+    		$data = $response->getBody();
+			
+			$data = json_decode($data);
+			dd($data);
+			'AccessToken' => 'key',
+				'Authorization' => 'Bearer token',
+			
 
-			$data = $response->getBody();
-			$rr=$data->getContents();
-			$data1 = json_decode($data,true);
-			dd($rr);
-		
+				$headers = [
+				'Content-Type' => 'application/json',
+				'Accept' => 'application/vnd.BNM.API.v1+json',
+				
+				];
+
+				$client = new GuzzleClient([
+				'headers' => $headers
+				]);
+
+			
+
+				$r = $client->Request('GET', 'https://api.bnm.gov.my/public/base-rate' ,[
+    'headers' => [
+        'Accept' => 'application/vnd.BNM.API.v1+json',
+        		],
+    ]);
+				$response = $r->getBody()->getContents();
+					dd($response);
+
+			*/
+
+
+  }
+  public function getRemoteData2(){
+
+    	$headers = [
+            'Accept' => "application/vnd.BNM.API.v1+json",
+            'User-Agent'=>'PostmanRuntime/7.16.3',
+        ];
+
+        $client = new Client([
+            'headers' => $headers
+        ]);
+
+        $r = $client->request('GET', 'https://api.bnm.gov.my/public/base-rate');
+        $response = $r->getBody()->getContents();
+         echo $response;
     	
 
     
